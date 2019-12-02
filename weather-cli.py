f5d5ea134372a653fb0f3d5c7d6337747f02ce25
Sobrino Julian <sobrinojulian@protonmail.com>
2019-12-02 20:16:44 -0300

    Mugriento pero imprime todo

--- weather-cli.py
+++ weather-cli.py
@@ -24,32 +24,45 @@ def get_forecast(location_id, forecast_json):
         if location["location_id"] == location_id:
             return location["forecast"]
 
-def filter_forecast(forecast):
-    d = {}
-    for k, v in forecast.items():
-        d[k] = {
+def print_forecast(forecast):
+    l = []
+    for k, v in sorted(forecast.items()):
+        l.append({
             "date": forecast[k]["date"],
             "temp_max": forecast[k]["temp_max"],
             "temp_min": forecast[k]["temp_min"],
-            "morning_description": forecast[k]["morning"]["description"],
-            "afternoon_description": forecast[k]["afternoon"]["description"]
-        }
-    return d
+            "morning_description": '\n'.join(forecast[k]["morning"]["description"].split('. ')),
+            "afternoon_description": '\n'.join(forecast[k]["afternoon"]["description"].split('. '))
+        })
+    for day in l:
+        if l[0] == day: continue
+        print(  f'{day["date"]}\n'
+                f'{day["temp_max"]}° / {day["temp_min"]}°\n'
+                f'{day["morning_description"]}\n'
+                f'{day["afternoon_description"]}'
+                f'\n')
 
+def format_weather(location_name, weather):
+    return (f'{location_name}\n'
+            f'{weather["description"]}\n'
+            f'{weather["tempDesc"]}\n'
+            f'{weather["wing_deg"]} {weather["wind_speed"]} km/h')
 
 def main():
     ipinfo_json = fetch_json("https://ipinfo.io/json")
     weather_json = fetch_json("https://ws.smn.gob.ar/map_items/weather")
     forecast_json = fetch_json("https://ws.smn.gob.ar/forecast")
 
-    name = ipinfo_json["city"]
-    location_id = get_location_id(name, weather_json)
+    location_name = ipinfo_json["city"]
+    location_id = get_location_id(location_name, weather_json)
 
     weather = get_weather(location_id, weather_json)
     forecast = get_forecast(location_id, forecast_json)
-    forecast = filter_forecast(forecast)
-    print(json.dumps(weather, indent=2, sort_keys=True))
 
-    print(json.dumps(forecast, indent=2, sort_keys=True))
+    print(format_weather(location_name, weather))
+    print()
+    print_forecast(forecast)
+    #print(json.dumps(weather, indent=2, sort_keys=True))
+    #print(json.dumps(forecast, indent=2, sort_keys=True))
 
 main()
